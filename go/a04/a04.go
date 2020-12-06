1f22833ec1c82c4d93962444488ccbaca4a41441
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-12-06 06:00:58 +0100

    go ahead

--- go/a04/a04.go
+++ go/a04/a04.go
@@ -3,77 +3,139 @@ package a04
 import (
 	"bufio"
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
-	"log"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
 type Passport struct {
-	byr string // byr (Birth Year)
-	cid string // cid (Country ID)
-	ecl string // ecl (Eye Color)
-	eyr string // eyr (Expiration Year)
-	hcl string // hcl (Hair Color)
-	hgt string // hgt (Height)
-	iyr string // iyr (Issue Year)
-	pid string // pid (Passport ID)
+	BirthYear      int // byr (Birth Year)
+	IssueYear      int // iyr (Issue Year)
+	ExpirationYear int // eyr (Expiration Year)
+
+	Height string // hgt (Height)
+
+	EyeColor  string // ecl (Eye Color)
+	HairColor string // hcl (Hair Color)
+
+	PassportID string // pid (Passport ID)
+	CountryID  string // cid (Country ID)
+
+	PrintErr bool
 }
 
-func (p *Passport) valid() bool {
-	if p.byr == "" {
-		return false
+var (
+	hgtRegex = *regexp.MustCompile(`(\d+)(\w+)`)
+	hclRegex = *regexp.MustCompile(`^#[a-f0-9]{6}$`)
+	pidRegex = *regexp.MustCompile(`[0-9]{9}`)
+)
+
+func (p *Passport) Validate() error {
+	errs := make([]error, 0)
+	appendErr := func(err error) {
+		if err != nil {
+			errs = append(errs, err)
+		}
 	}
-	if p.ecl == "" {
-		return false
+	if p.BirthYear < 1920 || p.BirthYear > 2020 {
+		appendErr(fmt.Errorf("invalid birth year: %v", p.BirthYear))
 	}
-	if p.eyr == "" {
-		return false
+	if p.IssueYear < 2010 || p.IssueYear > 2030 {
+		appendErr(fmt.Errorf("invalid issue year: %v", p.IssueYear))
 	}
-	if p.hcl == "" {
-		return false
+	if p.ExpirationYear < 2020 || p.ExpirationYear > 2030 {
+		appendErr(fmt.Errorf("invalid expiration year: %v", p.ExpirationYear))
 	}
-	if p.hgt == "" {
-		return false
+
+	// validate height
+	validateHgt := func(hgt string) error {
+		if hgt == "" {
+			return errors.New("height is required")
+		}
+		hgtparts := hgtRegex.FindStringSubmatch(hgt)
+		if hgtparts == nil || len(hgtparts) != 3 {
+			return fmt.Errorf("failed to parse height: %v resulted in %v", p.Height, hgtparts)
+		}
+		height, err := strconv.Atoi(hgtparts[1])
+		if err != nil {
+			return fmt.Errorf("failed to convert height %v to an int: %v", hgtparts[1], err)
+		}
+		unit := hgtparts[2]
+		switch unit {
+		case "cm":
+			if height < 150 || height > 193 {
+				return errors.New("height must be 150cm <= height <= 193cm")
+			}
+		case "in":
+			if height < 59 || height > 76 {
+				return errors.New("height must be 59in <= height <= 76in")
+			}
+		default:
+			return fmt.Errorf("unknown height unit %v", unit)
+		}
+		return nil
 	}
-	if p.iyr == "" {
-		return false
+	appendErr(validateHgt(p.Height))
+
+	// Hair color
+	validateHcl := func(hcl string) error {
+		if hcl == "" {
+			return errors.New("hair color is required")
+		}
+		if !hclRegex.MatchString(hcl) {
+			return errors.New("hair color did not match color regex")
+		}
+		return nil
 	}
-	if p.pid == "" {
-		return false
+	appendErr(validateHcl(p.HairColor))
+
+	// Eye color
+	switch p.EyeColor {
+	case "amb", "blu", "brn", "gry", "grn", "hzl", "oth":
+		break
+	default:
+		appendErr(fmt.Errorf("invalid eye color: %v", p.EyeColor))
 	}
-	if p.cid == "" {
-		return true
+
+	// Passport ID
+	if !pidRegex.MatchString(p.PassportID) {
+		appendErr(fmt.Errorf("passport id %v did not match regex", p.PassportID))
 	}
-	return true
+	if len(errs) > 0 {
+		compositeErr := errs[0]
+		for _, err := range errs[1:] {
+			compositeErr = fmt.Errorf("%v\n%w", err, compositeErr)
+		}
+		return compositeErr
+	}
+	return nil
 }
 
 func ParsePassportRow(row string) (p Passport) {
 	passportParts := strings.Split(row, " ")
 	for _, part := range passportParts {
-		k, v := part[:3], part[3:]
+		fieldParts := strings.Split(part, ":")
+		k, v := fieldParts[0], fieldParts[1]
 		switch k {
 		case "byr":
-			p.byr = v
-		case "cid":
-			p.cid = v
-		case "ecl":
-			p.ecl = v
-		case "eyr":
-			p.eyr = v
-		case "hcl":
-			p.hcl = v
-		case "hgt":
-			p.hgt = v
+			p.BirthYear, _ = strconv.Atoi(v)
 		case "iyr":
-			p.iyr = v
+			p.IssueYear, _ = strconv.Atoi(v)
+		case "eyr":
+			p.ExpirationYear, _ = strconv.Atoi(v)
+		case "cid":
+			p.CountryID = v
+		case "ecl":
+			p.EyeColor = v
+		case "hcl":
+			p.HairColor = v
+		case "hgt":
+			p.Height = v
 		case "pid":
-			p.pid = v
+			p.PassportID = v
 		default:
 			panic("unknown field " + v)
 		}
@@ -86,9 +148,12 @@ func Run(r io.Reader) (nvalid int) {
 	sc.Split(ScanPassport)
 	for sc.Scan() {
 		p := ParsePassportRow(sc.Text())
-		if p.valid() {
-			nvalid++
+		fmt.Printf("%+v\n", p)
+		if err := p.Validate(); err != nil {
+			fmt.Printf("INVALID! err: %v", err)
+			continue
 		}
+		nvalid++
 	}
 	return nvalid
 }
@@ -99,23 +164,21 @@ func ScanPassport(data []byte, atEOF bool) (advance int, token []byte, err error
 	}
 
 	i := bytes.IndexByte(data, '\n')
-	for {
-		j := bytes.IndexByte(data[i+1:], '\n')
-		// fmt.Println("----")
-		// fmt.Println(string(data[i+1:]))
-		// fmt.Println("----")
-		// fmt.Println("i", i, "j", j)
-		if j == -1 {
-			break
+	if i != -1 {
+		for {
+			j := bytes.IndexByte(data[i+1:], '\n')
+			if j == -1 {
+				break
+			}
+			if j == 0 {
+				return i + 2, bytes.ReplaceAll(data[:i], []byte{'\n'}, []byte{' '}), nil
+			}
+			i = i + j + 1
 		}
-		if j == 0 {
-			return i + 2, bytes.ReplaceAll(data[:i], []byte{'\n'}, []byte{' '}), nil
-		}
-		i = i + j + 1
 	}
 
 	if atEOF {
-		return len(data), bytes.ReplaceAll(data[:len(data)-1], []byte{'\n'}, []byte{' '}), nil
+		return len(data), bytes.ReplaceAll(data, []byte{'\n'}, []byte{' '}), nil
 	}
 
 	return 0, nil, nil
--- go/a04/a04_test.go
+++ go/a04/a04_test.go
@@ -4,6 +4,7 @@ import (
 	"aoc2020/a04"
 	"bufio"
 	"bytes"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -36,4 +37,70 @@ func Test_ScanPassport(t *testing.T) {
 	require.Equal(t, `hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm`, s.Text())
 	s.Scan()
 	require.Equal(t, `hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in`, s.Text())
+}
+
+var invalidPassports = `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`
+
+var validPassports = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
+
+func Test_Validation(t *testing.T) {
+	f := bytes.NewBufferString(invalidPassports)
+	sc := bufio.NewScanner(f)
+	sc.Split(a04.ScanPassport)
+	for sc.Scan() {
+		p := a04.ParsePassportRow(sc.Text())
+		validationErr := p.Validate()
+		if validationErr == nil {
+			t.Errorf("%+v was valid but should be invalid", p)
+		}
+	}
+	// Valid passports
+	f = bytes.NewBufferString(validPassports)
+	sc = bufio.NewScanner(f)
+	sc.Split(a04.ScanPassport)
+	for sc.Scan() {
+		p := a04.ParsePassportRow(sc.Text())
+		validationErr := p.Validate()
+		if validationErr != nil {
+			t.Errorf("%+v was invalid but should be valid, err: %v", p, validationErr)
+		}
+	}
+}
+
+func Test_Something(t *testing.T) {
+	hclRegex := *regexp.MustCompile(`^#[a-f0-9]{6}$`)
+	for _, tc := range []struct {
+		in       string
+		expected bool
+	}{
+		{`a6f86d`, false},
+		{`#ceb3a1`, true},
+	} {
+		t.Run(tc.in, func(t *testing.T) {
+			require.Equal(t, tc.expected, hclRegex.MatchString(tc.in))
+		})
+	}
 }
