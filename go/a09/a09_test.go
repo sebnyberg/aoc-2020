de2698f346618ee285765460a47f90908a39fde1
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-12-11 22:19:41 +0100

    done with day 9

--- go/a09/a09_test.go
+++ go/a09/a09_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"testing"
 
@@ -58,11 +59,14 @@ func Test_day9(t *testing.T) {
 	// iterate over all arrays from index 1:end and check if there is a match
 	npreamble := 25
 	ns := make([][]int, 0)
+	ns2 := make([]int, 0)
 	var i int
+	offender := 0
 	for sc.Scan() {
 		// Parse n
 		n, err := strconv.Atoi(sc.Text())
 		check(err)
+		ns2 = append(ns2, n)
 
 		// if we are past the preamble, check if the number has a sum
 		if i >= npreamble {
@@ -76,8 +80,8 @@ func Test_day9(t *testing.T) {
 				}
 			}
 			// No match found, exit!
-			fmt.Println("failed to find match for number", n)
-			printArr(ns)
+			offender = n
+			// printArr(ns)
 			break
 		}
 	EndCheck:
@@ -90,7 +94,16 @@ func Test_day9(t *testing.T) {
 		i++
 	}
 
-	require.Equal(t, ns, "")
+	require.Equal(t, 31161678, offender)
+
+	if offender == 0 {
+		panic("did not find offender!")
+	}
+	// Day 2
+	cont := findContiguousSet(ns2, offender)
+	// sort set, so that min is first and max last
+	sort.Ints(cont)
+	require.Equal(t, 5453868, cont[0]+cont[len(cont)-1])
 }
 
 func printArr(arr [][]int) {
@@ -101,4 +114,62 @@ func printArr(arr [][]int) {
 		}
 		fmt.Print("\n")
 	}
+}
+
+func Test_contiguousSet(t *testing.T) {
+	for _, tc := range []struct {
+		in   []int
+		s    int
+		want []int
+	}{
+		{nil, 3, nil},
+		{[]int{}, 3, nil},
+		{[]int{1, 2}, 3, []int{1, 2}},
+		{[]int{1, 2, 3, 4, 5}, 12, []int{3, 4, 5}},
+		{[]int{35, 20, 15, 25, 47, 40, 62, 55}, 127, []int{15, 25, 47, 40}},
+	} {
+		t.Run(fmt.Sprintf("%v", tc.in), func(t *testing.T) {
+			got := findContiguousSet(tc.in, tc.s)
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func findContiguousSet(in []int, s int) []int {
+	if len(in) < 2 {
+		return nil
+	}
+	var i, j int
+	// starting point
+	var cursum = in[0]
+	// edge case
+	// Re-slice the array based on i, j until j = len(s)-1 and sum < s
+	// Then there is no "moves" left that could find the target anymore.
+	for {
+		// While the current sum is too small, move ahead, adding numbers
+		for cursum < s && j < len(in) {
+			j++
+			cursum += in[j]
+		}
+		if cursum == s {
+			break
+		}
+		// The sum is now too great, or j == len(in)
+		// Subtract from the tail until we are below the sum, or at stop condition
+		for cursum > s && i < len(in)-1 {
+			cursum -= in[i]
+			i++
+		}
+		if cursum == s {
+			break
+		}
+
+		// Stop condition
+		if i == len(in)-1 {
+			return nil
+		}
+	}
+	res := make([]int, j-i+1)
+	copy(res, in[i:j+1])
+	return res
 }
