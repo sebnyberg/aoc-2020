fba6567b52b40efc0fb120bcc1f5c77560e16f6a
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-12-14 17:25:43 +0100

    a14 part 1 done

--- go/a14/a14_test.go
+++ go/a14/a14_test.go
@@ -1,31 +1,71 @@
 package a14_test
 
 import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func Test_parseInput(t *testing.T) {
-	testCases := []struct {
-		input string
-		want  string
-	}{
-		{"mem[4616] = 8311689", "mem"},
-		{"mem[4616] = 8311689", "mem"},
-		{"mem[4616] = 8311689", "mem"},
-	}
-
-	for _, testCase := range testCases {
-		t.Run(testCase.input, func(t *testing.T) {
-			got := parseInput(testCase.input)
-			if got != testCase.want {
-				require.Equal(t, testCase.want, got)
-			}
-		})
+func check(err error) {
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
 
-func parseInput(input string) string {
-	return "mem"
+func Test_parseInput(t *testing.T) {
+	f, err := os.Open("input")
+	check(err)
+
+	mem := make(map[int]int, 1000)
+	oneMask := 0
+	zeroMask := 0
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		row := sc.Text()
+		parts := strings.Split(row, " = ")
+		instr, valstr := parts[0], parts[1]
+		if instr == "mask" {
+			// Clear
+			oneMask = 0
+			zeroMask = 0
+			for i, ch := range valstr {
+				switch ch {
+				case 'X':
+					continue
+				case '1':
+					oneMask |= 1 << (35 - i)
+				case '0':
+					zeroMask |= 1 << (35 - i)
+				default:
+					panic(ch)
+				}
+			}
+			continue
+		}
+
+		// "mem" instruction
+		addrstr := strings.TrimRight(strings.TrimLeft(instr, "me["), "]")
+		addr, err := strconv.Atoi(addrstr)
+		check(err)
+		val, err := strconv.Atoi(valstr)
+		check(err)
+
+		// Apply mask
+		val |= oneMask
+		val &= ^zeroMask
+		mem[addr] = val
+	}
+
+	sum := 0
+	for _, val := range mem {
+		sum += val
+	}
+	require.Equal(t, 0, sum)
+
+	t.FailNow()
 }
