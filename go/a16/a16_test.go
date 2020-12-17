844aa15378d5087d3a9c85b73629affcbab72edd
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-12-17 07:32:01 +0100

    start on 17

--- go/a16/a16_test.go
+++ go/a16/a16_test.go
@@ -1,157 +1,165 @@
 package a16_test
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-	"testing"
+// type RangeRule struct {
+// 	start int
+// 	end   int
+// }
 
-	"github.com/stretchr/testify/require"
-)
+// func Test_day16(t *testing.T) {
+// 	f, err := os.Open("input")
+// 	check(err)
+// 	sc := bufio.NewScanner(f)
+// 	validations := make(map[string][]RangeRule)
 
-type RangeRule struct {
-	start int
-	end   int
-}
+// 	// scan rules
+// 	for sc.Scan() {
+// 		row := sc.Text()
+// 		if row == "" {
+// 			break
+// 		}
+// 		ruleparts := strings.Split(row, ":")
+// 		ruleName := ruleparts[0]
+// 		ruleranges := strings.Split(ruleparts[1], "or")
+// 		for _, rulerange := range ruleranges {
+// 			rangeparts := strings.Split(strings.Trim(rulerange, " "), "-")
+// 			start, err := strconv.Atoi(rangeparts[0])
+// 			check(err)
+// 			end, err := strconv.Atoi(rangeparts[1])
+// 			check(err)
+// 			if _, exists := validations[ruleName]; exists {
+// 				validations[ruleName] = append(validations[ruleName], RangeRule{start, end})
+// 				continue
+// 			}
+// 			validations[ruleName] = []RangeRule{{start, end}}
+// 		}
+// 	}
 
-func Test_day16(t *testing.T) {
-	f, err := os.Open("input")
-	check(err)
-	sc := bufio.NewScanner(f)
-	validations := make(map[string][]RangeRule)
+// 	// Scan your ticket
+// 	// var ticket []int
+// 	sc.Scan() // Skip header
+// 	for sc.Scan() {
+// 		row := sc.Text()
+// 		if row == "" {
+// 			break
+// 		}
+// 		// ticket = parseTicket(row)
+// 	}
 
-	// scan rules
-	for sc.Scan() {
-		row := sc.Text()
-		if row == "" {
-			break
-		}
-		ruleparts := strings.Split(row, ":")
-		ruleName := ruleparts[0]
-		ruleranges := strings.Split(ruleparts[1], "or")
-		for _, rulerange := range ruleranges {
-			rangeparts := strings.Split(strings.Trim(rulerange, " "), "-")
-			start, err := strconv.Atoi(rangeparts[0])
-			check(err)
-			end, err := strconv.Atoi(rangeparts[1])
-			check(err)
-			if _, exists := validations[ruleName]; exists {
-				validations[ruleName] = append(validations[ruleName], RangeRule{start, end})
-				continue
-			}
-			validations[ruleName] = []RangeRule{{start, end}}
-		}
-	}
+// 	// Scan nearby tickets
+// 	nearbyTickets := make([][]int, 0)
+// 	sc.Scan() // Skip header
+// 	for sc.Scan() {
+// 		row := sc.Text()
+// 		if row == "" {
+// 			break
+// 		}
+// 		nearbyTickets = append(nearbyTickets, parseTicket(row))
+// 	}
 
-	// Scan your ticket
-	// var ticket []int
-	sc.Scan() // Skip header
-	for sc.Scan() {
-		row := sc.Text()
-		if row == "" {
-			break
-		}
-		// ticket = parseTicket(row)
-	}
+// 	// Validate nearby tickets
+// 	invalidFields := make([]int, 0)
+// 	validTickets := make([][]int, 0)
+// 	for _, nearbyTicket := range nearbyTickets {
+// 		invalidFieldsForTicket := validateTicket(nearbyTicket, validations)
+// 		if len(invalidFieldsForTicket) == 0 {
+// 			validTickets = append(validTickets, nearbyTicket)
+// 			continue
+// 		}
+// 		invalidFields = append(invalidFields, invalidFieldsForTicket...)
+// 	}
 
-	// Scan nearby tickets
-	nearbyTickets := make([][]int, 0)
-	sc.Scan() // Skip header
-	for sc.Scan() {
-		row := sc.Text()
-		if row == "" {
-			break
-		}
-		nearbyTickets = append(nearbyTickets, parseTicket(row))
-	}
+// 	// Part 1
+// 	var sum int
+// 	for _, invalidField := range invalidFields {
+// 		sum += invalidField
+// 	}
+// 	require.Equal(t, 20058, sum)
 
-	// Validate nearby tickets
-	invalidFields := make([]int, 0)
-	validTickets := make([][]int, 0)
-	for _, nearbyTicket := range nearbyTickets {
-		invalidFieldsForTicket := validateTicket(nearbyTicket, validations)
-		if len(invalidFieldsForTicket) == 0 {
-			validTickets = append(validTickets, nearbyTicket)
-			continue
-		}
-		invalidFields = append(invalidFields, invalidFieldsForTicket...)
-	}
+// 	// Part 2
+// 	ruleNameValidForField := make(map[string]int)
 
-	// Part 1
-	var sum int
-	for _, invalidField := range invalidFields {
-		sum += invalidField
-	}
-	require.Equal(t, 20058, sum)
+// 	// Reorder tickets by field
+// 	ticketByField := make([][]int, len(validTickets[0]))
+// 	for _, validTicket := range validTickets {
+// 		for i := 0; i < len(validTickets[0]); i++ {
+// 			ticketByField[i] = append(ticketByField[i], validTicket[i])
+// 		}
+// 	}
+// 	require.Equal(t, validTickets[0][0], ticketByField[0][0])
+// 	require.Equal(t, validTickets[0][1], ticketByField[1][0])
+// 	require.Equal(t, validTickets[len(validTickets)-1][0], ticketByField[0][len(validTickets)-1])
 
-	// Part 2
-	ruleNameValidForField := make(map[string]int)
+// 	// For each field
+// 	for i, fieldValues := range ticketByField {
+// 		validForAll := true
+// 		for _, fieldRules := range validations {
+// 			invalidFieldsForTicket := validateTicket(nearbyTicket, validations)
+// 			if len(invalidFieldsForTicket) == 0 {
+// 				validTickets = append(validTickets, nearbyTicket)
+// 				continue
+// 			}
+// 		}
+// 		invalidFields = append(invalidFields, invalidFieldsForTicket...)
+// 		for _, fieldValue := range fieldValues {
+// 			// For each rule
+// 			for _, fieldRules := range validations {
+// 				for _, rangeRules := range fieldRules {
+// 				}
+// 			}
+// 		}
+// 	}
 
-	// For each valid ticket and field, check which rule applies
-	for ticketEntryIdx := 0; ticketEntryIdx < len(validTickets[0]); ticketEntryIdx++ {
-		// For each rule
-		for fieldName, fieldRules := range validations {
-			validForAll := true
-			// For each ticket
-			for _, validTicket := range validTickets {
-				// If none of the range rules apply, continue to next field
-				validForTicket := false
-				for _, rangeRules := range fieldRules {
-					if validTicket[ticketEntryIdx] >= rangeRules.start && validTicket[ticketEntryIdx] <= rangeRules.end {
-						validForTicket = true
-						break
-					}
-				}
-				if !validForTicket {
-					validForAll = false
-					break
-				}
-			}
-			if validForAll {
-				ruleNameValidForField[fieldName] = ticketEntryIdx
-			}
-		}
-	}
+// 	fmt.Println(ruleNameValidForField)
+// 	t.FailNow()
+// }
 
-	fmt.Println(ruleNameValidForField)
-	t.FailNow()
-}
+// func fieldsValid(rules []RangeRule, fieldValue int, fieldValues ...int) bool {
+// 	if len(fieldValues) == 0 {
+// 		fieldValues = []int{fieldValue}
+// 	} else {
+// 		fieldValues = append(fieldValues, fieldValue)
+// 	}
+// 	// if no rule matches, return false
+// 	for _, rule := range rules {
+// 		for _, fieldValue := range fieldValues {
+// 			if fieldValue <=
+// 		}
+// 	}
+// 	return false
+// }
 
-func validateTicket(ticket []int, validations map[string][]RangeRule) []int {
-	invalidFields := make([]int, 0)
-	for _, ticketEntry := range ticket {
-		isvalid := false
-		for _, validation := range validations {
-			for _, rule := range validation {
-				if ticketEntry >= rule.start && ticketEntry <= rule.end {
-					isvalid = true
-					break
-				}
-			}
-		}
-		if !isvalid {
-			invalidFields = append(invalidFields, ticketEntry)
-		}
-	}
-	return invalidFields
-}
+// func validateTicket(ticket []int, validations map[string][]RangeRule) []int {
+// 	invalidFields := make([]int, 0)
+// 	for _, ticketEntry := range ticket {
+// 		isvalid := false
+// 		for _, validation := range validations {
+// 			for _, rule := range validation {
+// 				if ticketEntry >= rule.start && ticketEntry <= rule.end {
+// 					isvalid = true
+// 					break
+// 				}
+// 			}
+// 		}
+// 		if !isvalid {
+// 			invalidFields = append(invalidFields, ticketEntry)
+// 		}
+// 	}
+// 	return invalidFields
+// }
 
-func parseTicket(row string) []int {
-	ticket := make([]int, 0)
-	rownums := strings.Split(row, ",")
-	for _, rownumstr := range rownums {
-		n, err := strconv.Atoi(rownumstr)
-		check(err)
-		ticket = append(ticket, n)
-	}
-	return ticket
-}
+// func parseTicket(row string) []int {
+// 	ticket := make([]int, 0)
+// 	rownums := strings.Split(row, ",")
+// 	for _, rownumstr := range rownums {
+// 		n, err := strconv.Atoi(rownumstr)
+// 		check(err)
+// 		ticket = append(ticket, n)
+// 	}
+// 	return ticket
+// }
 
-func check(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
+// func check(err error) {
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// }
