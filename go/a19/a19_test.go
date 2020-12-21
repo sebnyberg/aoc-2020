c3e56775f50525d31cdd85c9c0350f5b16496eb2
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-12-21 11:06:39 +0100

    Done with day19

--- go/a19/a19_test.go
+++ go/a19/a19_test.go
@@ -2,6 +2,7 @@ package a19_test
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -13,7 +14,7 @@ import (
 )
 
 func Test_ruleMatcher(t *testing.T) {
-	f, err := os.Open("testinput3")
+	f, err := os.Open("input")
 	check(err)
 	sc := bufio.NewScanner(f)
 	rows := make([]string, 0)
@@ -25,16 +26,15 @@ func Test_ruleMatcher(t *testing.T) {
 	}
 	m := parse(rows)
 
-	require.Equal(t, true, m.match("aaaaabbaabaaaaababaa"))
-	// var ntot int
-	// for sc.Scan() {
-	// 	input := sc.Text()
-	// 	if m.match(input) {
-	// 		ntot++
-	// 	}
-	// }
-	// require.Equal(t, 0, ntot)
-	// t.FailNow()
+	var ntot int
+	for sc.Scan() {
+		input := sc.Text()
+		if m.match(input) {
+			ntot++
+		}
+	}
+	require.Equal(t, 0, ntot)
+	t.FailNow()
 }
 
 type ruleFn func(s string) (matched bool, width int)
@@ -44,11 +44,27 @@ type ruleMatcher struct {
 }
 
 func (m ruleMatcher) match(s string) bool {
-	matched, nmatched := m.rules[0](s)
-	if nmatched < len(s) {
-		return false
+	// Custom rule 0... just want to finish this day
+	rule8alts := strings.Split(genRecursions("8", "42 | 42 8", len(s)-2), "|")
+	rule11alts := strings.Split(genRecursions("11", "42 31 | 42 11 31", len(s)-1), "|")
+	rule0alts := make([]string, 0)
+	for _, rule8alt := range rule8alts {
+		for _, rule11alt := range rule11alts {
+			rule0 := strings.Trim(rule8alt, " ") + " " + strings.Trim(rule11alt, " ")
+			if len(rule0) > len(s) {
+				continue
+			}
+			rule0alts = append(rule0alts, rule0)
+		}
 	}
-	return matched
+	for _, rule0alt := range rule0alts {
+		m.rules[0] = newOrRule(rule0alt, m.rules)
+		match, nmatched := m.rules[0](s)
+		if match && nmatched >= len(s) {
+			return true
+		}
+	}
+	return false
 }
 
 func parse(ruleRows []string) ruleMatcher {
@@ -71,35 +87,96 @@ func parse(ruleRows []string) ruleMatcher {
 		}
 
 		// Parse rule referencer (and/or)
-		m.rules[id] = func(s string) (matched bool, nmatched int) {
-			orParts := strings.Split(assignParts[1], "|")
-			var newmatched int
-			for _, orPart := range orParts {
-				matched = false
-				nmatched = 0
-
-				andParts := strings.Split(strings.Trim(orPart, " "), " ")
-				for _, andPart := range andParts {
-					newmatched = 0
-					ruleID, err := strconv.Atoi(andPart)
-					check(err)
-					if nmatched >= len(s) {
-						return false, nmatched
-					}
-					matched, newmatched = m.rules[ruleID](s[nmatched:])
-					if !matched {
-						break
-					}
-					nmatched += newmatched
-				}
-				if matched {
-					return true, nmatched
-				}
-			}
-			return false, 0
-		}
+		m.rules[id] = newOrRule(assignParts[1], m.rules)
 	}
 	return m
+}
+
+func newOrRule(rule string, otherRules map[int]ruleFn) ruleFn {
+	return func(s string) (matched bool, nmatched int) {
+		orParts := strings.Split(rule, "|")
+		var newmatched int
+		for i := 0; i < len(orParts); i++ {
+			matched = false
+			nmatched = 0
+			orPart := orParts[i]
+
+			andParts := strings.Split(strings.Trim(orPart, " "), " ")
+			for _, andPart := range andParts {
+				newmatched = 0
+				ruleID, err := strconv.Atoi(andPart)
+				check(err)
+				if nmatched >= len(s) {
+					return false, nmatched
+				}
+				matched, newmatched = otherRules[ruleID](s[nmatched:])
+				if !matched {
+					break
+				}
+				nmatched += newmatched
+			}
+
+			if matched {
+				return true, nmatched
+			}
+
+		}
+		return false, 0
+	}
+}
+func Test_genRecursions(t *testing.T) {
+	for _, tc := range []struct {
+		id     string
+		rule   string
+		maxlen int
+		want   string
+	}{
+		{"8", "9 10", 10, "9 10"},
+		{"8", "9 10 | 11 | 12", 10, "9 10 | 11 | 12"},
+		{"8", "42 | 42 8", 3, "42 | 42 42 | 42 42 42"},
+		{"8", "42 | 42 8", 4, "42 | 42 42 | 42 42 42 | 42 42 42 42"},
+		{"11", "42 31 | 42 11 31", 3, "42 31"},
+		{"11", "42 31 | 42 11 31", 4, "42 31 | 42 42 31 31"},
+		{"11", "42 31 | 42 11 31", 6, "42 31 | 42 42 31 31 | 42 42 42 31 31 31"},
+	} {
+		t.Run(fmt.Sprintf("%v/%v/%v", tc.id, tc.rule, tc.maxlen), func(t *testing.T) {
+			got := genRecursions(tc.id, tc.rule, tc.maxlen)
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func genRecursions(ruleID string, rule string, maxlen int) string {
+	if !strings.Contains(rule, ruleID) ||
+		!strings.ContainsRune(rule, '|') {
+		return rule
+	}
+	orParts := strings.Split(rule, " | ")
+	// The terminator ends the recurrence pattern
+	terminator := strings.Trim(orParts[0], " ")
+	recurser := strings.Trim(orParts[1], " ")
+	terminatorLen := len(strings.Split(terminator, " "))
+	curLen := len(strings.Split(strings.Trim(orParts[1], " "), " ")) - 1
+	if maxlen < curLen+terminatorLen {
+		return orParts[0]
+	}
+
+	for i := 1; ; i++ {
+		// Copy and add last part to the end of the list
+		orParts = append(orParts, orParts[i])
+
+		// Replace ruleID with last part within new last part
+		orParts[i+1] = strings.Replace(orParts[i], ruleID, recurser, 1)
+
+		// Replace ruleID in current
+		orParts[i] = strings.Replace(orParts[i], ruleID, terminator, 1)
+
+		curLen += terminatorLen
+
+		if curLen >= maxlen {
+			return strings.Join(orParts[:len(orParts)-1], " | ")
+		}
+	}
 }
 
 func check(err error) {
